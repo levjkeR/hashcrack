7bf2c2f0738f4a07d74aec26a3c99f6f03b61762
levjke <levjke&@gmail.com>
2022-02-14 18:14:34 +0300

    Optimize imports/arguments

--- hashcrack.py
+++ hashcrack.py
@@ -1,27 +1,24 @@
-import multiprocessing
-import textwrap
-from hashlib import new
-
 import argparse
-
+import multiprocessing
+from hashlib import new
 from itertools import product
-import string
+
 parser = argparse.ArgumentParser(description="HashCrack Multiprocessing Tool",
                                  formatter_class=argparse.RawDescriptionHelpFormatter,
-                                 epilog=textwrap.dedent('''Example
-                                 hashcrack.py -n 6 -c lower -l 5 5 -a md5 -t 37b19816109a32106d109e83bbb3c97d
-                                 hashcrack.py -a sha256 -t 3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b
-                                 '''))
-parser.add_argument('-n', '--number', type=int, default=multiprocessing.cpu_count()//2,
+                                 epilog=('Example\nhashcrack.py -n 6 -c lower -l 5 5 -a md5 -t '
+                                         '37b19816109a32106d109e83bbb3c97d\nhashcrack.py -a sha256 -t '
+                                         '3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b'))
+parser.add_argument('-n', '--number', type=int, default=multiprocessing.cpu_count() // 2,
                     help='number of process')
-parser.add_argument('-c', '--charset', default='lower', help='charset: upper[A-Z], lower[a-z], digits[0-9], extended[A-za-z0-9]')
-parser.add_argument('-l', '--length', nargs="+", type=int, default=[5, 5], help='plain text length range')
+parser.add_argument('-c', '--charset', default='lower', help='upper[A-Z], lower[a-z], digits[0-9], extended[A-za-z0-9]')
+parser.add_argument('-l', '--length', nargs='*', type=int, default=[5, 5], help='plain text length range')
 parser.add_argument('-a', '--alg', help='hash function algorithm')
-parser.add_argument('-t', '--target', help='target hash')
+parser.add_argument('-t', '--target', help='target hash', required=True)
 
 args = parser.parse_args()
 if len(args.length) == 1:
     args.length = [args.length[0], args.length[0]]
+
 
 # Проверка текста на совпадение исходному тексту
 def check_hash(alg, target_hash, suspect_str):
@@ -92,7 +89,7 @@ def bruteforce(n, charset, lenrange, alg, target, chunk_size=1000000):
         for comb in product(charset, repeat=length - chunk_len):
             first_char = ''.join(comb)
             pool.apply_async(generator, args=(charset, first_char, chunk_len, length, alg, target),
-                                      callback=ext_callback)
+                             callback=ext_callback)
 
     pool.close()
     pool.join()
@@ -101,14 +98,16 @@ def bruteforce(n, charset, lenrange, alg, target, chunk_size=1000000):
 if __name__ == '__main__':
     from sys import platform
     import time
+
     if 'win' in platform:
         import ctypes
+
         kernel32 = ctypes.windll.kernel32
         kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
-    charset = {'lower': list(string.ascii_lowercase),
-               'digits': list(string.digits),
-               'upper': list(string.ascii_uppercase),
-               'extended': list(string.ascii_uppercase)+list(string.ascii_lowercase)+list(string.digits)}
+    charset = {'lower': 'abcdefghijklmnopqrstuvwxyz',
+               'digits': '0123456789',
+               'upper': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+               'extended': '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"#$%&\\'()*+,-./:;'''}
 
     print(f'''\033[32m
  ██░ ██  ▄▄▄        ██████  ██░ ██  ▄████▄   ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀
@@ -127,4 +126,4 @@ if __name__ == '__main__':
     start = time.time()
     bruteforce(n=args.number, charset=charset[str(args.charset)], lenrange=args.length,
                alg=args.alg, target=args.target)
-    print(f"\033[36m[*]\033[0m Elapsed time: {time.time()-start} sec")
+    print(f"\033[36m[*]\033[0m Elapsed time: {time.time() - start} sec")
